6433e4ba0f889d2a0e0b06a2544ad64f881fe71a
MA-Erik <e.vanwees@ma-web.nl>
2019-09-19 00:00:10 +0200

    B1L1 Eindresultaat Upload

---
+++ Blok_1/B1L1_Eindresultaat/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _Eindresultaat
+{
+    class Program
+    {
+
+        // ------------------ ROCK/PAPER/SCISSORS RPG ------------------
+        static void Main(string[] args)
+        {
+
+
+            // ---------- WELCOME SCREEN, DO ONCE -------------
+            
+            Console.Clear();
+
+            Console.WriteLine("########################################################################");
+            Console.WriteLine("########################################################################");
+            Console.WriteLine("###########                                                  ###########");
+            Console.WriteLine("###########          WELCOME TO:  'SLAY THE DEMON'!          ###########");
+            Console.WriteLine("###########                                                  ###########");
+            Console.WriteLine("###########            PRESS ANY KEY TO CONTINUE.            ###########");
+            Console.WriteLine("###########                                                  ###########");
+            Console.WriteLine("########################################################################");
+            Console.WriteLine("########################################################################");
+            
+            // -------------------------------------------------
+            
+            Console.ReadLine();
+
+
+
+            // END GAME:
+            Console.Clear();
+            Console.WriteLine("Thank you for playing! Press any button to exit.");
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
